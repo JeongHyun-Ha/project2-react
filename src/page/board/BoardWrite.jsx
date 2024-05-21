2c7ca3c5540143976e308e01f608179eaee69300
JeongHyun Ha <aszx2024@naver.com>
2024-05-21 14:08:53 +0900

    전송 중 버튼 비활성화

--- src/page/board/BoardWrite.jsx
+++ src/page/board/BoardWrite.jsx
@@ -17,8 +17,10 @@ export function BoardWrite() {
   const [writer, setWriter] = useState("");
   const toast = useToast();
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
   function handleSaveClick() {
+    setLoading(true);
     axios
       .post("/api/board/add", { title, content, writer })
       .then(() => {
@@ -40,7 +42,7 @@ export function BoardWrite() {
           });
         }
       })
-      .finally();
+      .finally(() => setLoading(false));
   }
 
   let disableSaveBtn = false;
@@ -75,6 +77,7 @@ export function BoardWrite() {
         </Box>
         <Box>
           <Button
+            isLoading={loading}
             isDisabled={disableSaveBtn}
             colorScheme={"blue"}
             onClick={handleSaveClick}
