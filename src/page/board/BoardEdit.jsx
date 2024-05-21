178474c74e6db575a21f8266b6d0879b6a6007cd
JeongHyun Ha <aszx2024@naver.com>
2024-05-21 16:42:00 +0900

    수정 내용 저장시 모달 출력

--- src/page/board/BoardEdit.jsx
+++ src/page/board/BoardEdit.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import {
@@ -7,13 +7,25 @@ import {
   FormControl,
   FormLabel,
   Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Spinner,
   Textarea,
+  useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
 
 export function BoardEdit() {
   const { id } = useParams();
   const [board, setBoard] = useState(null);
+  const toast = useToast();
+  const navigate = useNavigate();
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
   useEffect(() => {
     axios.get(`/api/board/${id}`).then((res) => setBoard(res.data));
   }, []);
@@ -23,7 +35,29 @@ export function BoardEdit() {
   }
 
   function handleClickSave() {
-    axios.put(`/api/board/edit`, board);
+    axios
+      .put(`/api/board/edit`, board)
+      .then(() => {
+        toast({
+          status: "success",
+          description: `${board.id}번 게시물이 수정이 되었습니다.`,
+          position: "top",
+        });
+        navigate(`/board/${board.id}`);
+      })
+      .catch((err) => {
+        if (err.response.status === 400) {
+          toast({
+            status: "error",
+            description:
+              "게시물이 수정되지 않았습니다. 작성한 내용을 확인해주세요.",
+            position: "top",
+          });
+        }
+      })
+      .finally(() => {
+        onClose();
+      });
   }
 
   return (
@@ -58,11 +92,24 @@ export function BoardEdit() {
           </FormControl>
         </Box>
         <Box>
-          <Button colorScheme={"blue"} onClick={handleClickSave}>
+          <Button colorScheme={"blue"} onClick={onOpen}>
             저장
           </Button>
         </Box>
       </Box>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>수정</ModalHeader>
+          <ModalBody>수정한 내용을 저장하시겠습니까?</ModalBody>
+          <ModalFooter>
+            <Button onClick={handleClickSave} colorScheme={"blue"}>
+              수정
+            </Button>
+            <Button onClick={onClose}>취소</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
