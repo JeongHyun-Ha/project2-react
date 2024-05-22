9067b5d7054a8c43630394ed9b31646bf1b0fa95
JeongHyun Ha <aszx2024@naver.com>
2024-05-22 16:11:21 +0900

    회원 정보 보기

--- src/page/member/MemberInfo.jsx
+++ src/page/member/MemberInfo.jsx
@@ -1,14 +1,68 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Spinner,
+  Textarea,
+  useToast,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
 
 export function MemberInfo() {
+  const { id } = useParams();
+  const [member, setMember] = useState(null);
+
+  const toast = useToast();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get(`/api/member/${id}`)
+      .then((res) => setMember(res.data))
+      .catch((err) => {
+        toast({
+          status: "error",
+          description: "존재하지 않는 게시물입니다.",
+          position: "top",
+        });
+        navigate("/member/list");
+      });
+  }, []);
+
+  if (member == null) {
+    return <Spinner />;
+  }
+
   return (
     <Box>
       <Box>회원정보</Box>
       <Box>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
+        <Box>
+          <FormControl>
+            <FormLabel>이메일</FormLabel>
+            <Input value={member.email} isReadOnly />
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl>
+            <FormLabel>닉네임</FormLabel>
+            <Textarea value={member.nickName} isReadOnly />
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl>
+            <FormLabel>가입일시</FormLabel>
+            <Input value={member.inserted} type={"datetime-local"} isReadOnly />
+          </FormControl>
+        </Box>
+        <Box>
+          <Button colorScheme={"blue"}>수정</Button>
+          <Button colorScheme={"red"}>삭제</Button>
+        </Box>
       </Box>
     </Box>
   );
