dcc027e34b1fac0f32fe4d7a29aa1b48fbba78f8
JeongHyun Ha <aszx2024@naver.com>
2024-05-23 14:37:40 +0900

    로그인 시도 후 응답 코드에 따른 로직 작성
    200 : localStorage에 토큰 저장 후 메인으로 이동
    나머지 : localStorage에서 토큰 삭제

--- src/page/member/MemberLogin.jsx
+++ src/page/member/MemberLogin.jsx
@@ -1,6 +1,44 @@
-import { Box, Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  useToast,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function MemberLogin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const toast = useToast();
+  const navigate = useNavigate();
+
+  function handleLogin() {
+    axios
+      .post("/api/member/token", { email, password })
+      .then((res) => {
+        localStorage.setItem("token", res.data.token);
+        toast({
+          status: "success",
+          description: "로그인 되었습니다.",
+          position: "top",
+        });
+      })
+      .catch(() => {
+        localStorage.removeItem("token");
+        toast({
+          status: "warning",
+          description: "이메일과 패스워드를 확인해주세요",
+          position: "top",
+        });
+      })
+      .finally();
+  }
+
   return (
     <Box>
       <Box>로그인</Box>
@@ -8,17 +46,19 @@ export function MemberLogin() {
         <Box>
           <FormControl>
             <FormLabel>이메일</FormLabel>
-            <Input />
+            <Input onChange={(e) => setEmail(e.target.value)} />
           </FormControl>
         </Box>
         <Box>
           <FormControl>
             <FormLabel>비밀번호</FormLabel>
-            <Input />
+            <Input onChange={(e) => setPassword(e.target.value)} />
           </FormControl>
         </Box>
         <Box>
-          <Button colorScheme={"blue"}>로그인</Button>
+          <Button onClick={handleLogin} colorScheme={"blue"}>
+            로그인
+          </Button>
         </Box>
       </Box>
     </Box>
