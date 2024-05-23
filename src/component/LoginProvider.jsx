71a6ae176dc4f89e66437c511a72005b442fa7a2
JeongHyun Ha <aszx2024@naver.com>
2024-05-23 15:31:19 +0900

    LoginProvider.jsx 에서 login, logout 처리
    MemberLogin.jsx, Navbar.jsx 에서 LoginProvider.jsx에 있는 login, logout 메소드 활용

---
+++ src/component/LoginProvider.jsx
@@ -0,0 +1,49 @@
+import React, { createContext, useState } from "react";
+import { jwtDecode } from "jwt-decode";
+
+export const LoginContext = createContext(null);
+
+export function LoginProvider({ children }) {
+  const [email, setEmail] = useState("");
+  const [nickName, setNickName] = useState("");
+  const [expired, setExpired] = useState(0);
+
+  function isLoggedIn() {
+    return Date.now() < expired * 1000;
+  }
+
+  // hasEmail
+  function hasEmail(param) {
+    return email === param;
+  }
+
+  function login(token) {
+    localStorage.setItem("token", token);
+    const payload = jwtDecode(token);
+    setExpired(payload.exp);
+    setEmail(payload.sub);
+    setNickName(payload.nickName);
+  }
+
+  function logout() {
+    localStorage.removeItem("token");
+    setExpired(0);
+    setEmail("");
+    setNickName("");
+  }
+
+  return (
+    <LoginContext.Provider
+      value={{
+        email,
+        nickName,
+        login,
+        logout,
+        isLoggedIn,
+        hasEmail,
+      }}
+    >
+      {children}
+    </LoginContext.Provider>
+  );
+}
--- src/page/member/MemberLogin.jsx
+++ src/page/member/MemberLogin.jsx
@@ -6,33 +6,35 @@ import {
   Input,
   useToast,
 } from "@chakra-ui/react";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { LoginContext } from "../../component/LoginProvider.jsx";
 
 export function MemberLogin() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
   const toast = useToast();
   const navigate = useNavigate();
+  const account = useContext(LoginContext);
 
   function handleLogin() {
     axios
       .post("/api/member/token", { email, password })
       .then((res) => {
-        localStorage.setItem("token", res.data.token);
+        account.login(res.data.token);
         toast({
           status: "success",
           description: "로그인 되었습니다.",
           position: "top",
         });
+        navigate("/");
       })
       .catch(() => {
-        localStorage.removeItem("token");
+        account.logout();
         toast({
           status: "warning",
-          description: "이메일과 패스워드를 확인해주세요",
+          description: "이메일과 비밀번호를 확인해주세요",
           position: "top",
         });
       })
