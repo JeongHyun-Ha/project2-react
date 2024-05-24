351697c2f7356dee4310d031ee8d18d8bf82a382
JeongHyun Ha <aszx2024@naver.com>
2024-05-24 11:09:54 +0900

    글 보기 화면의 수정/삭제 버튼이 자신의 글에만 보이기

--- src/page/board/BoardView.jsx
+++ src/page/board/BoardView.jsx
@@ -1,5 +1,5 @@
 import { useNavigate, useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import {
   Box,
@@ -18,10 +18,12 @@ import {
   useDisclosure,
   useToast,
 } from "@chakra-ui/react";
+import { LoginContext } from "../../component/LoginProvider.jsx";
 
 export function BoardView() {
   const { id } = useParams();
   const [board, setBoard] = useState(null);
+  const account = useContext(LoginContext);
   const toast = useToast();
   const navigate = useNavigate();
   const { isOpen, onClose, onOpen } = useDisclosure();
@@ -99,17 +101,19 @@ export function BoardView() {
             <Input type={"datetime-local"} value={board.inserted} readOnly />
           </FormControl>
         </Box>
-        <Box>
-          <Button
-            colorScheme={"blue"}
-            onClick={() => navigate(`/edit/${board.id}`)}
-          >
-            수정
-          </Button>
-          <Button colorScheme={"red"} onClick={onOpen}>
-            삭제
-          </Button>
-        </Box>
+        {account.hasAccess(board.memberId) && (
+          <Box>
+            <Button
+              colorScheme={"blue"}
+              onClick={() => navigate(`/edit/${board.id}`)}
+            >
+              수정
+            </Button>
+            <Button colorScheme={"red"} onClick={onOpen}>
+              삭제
+            </Button>
+          </Box>
+        )}
       </Box>
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
