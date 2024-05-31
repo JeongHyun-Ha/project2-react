88b2e67e0dd48d84af687b0cead9f2ff35271879
JeongHyun Ha <aszx2024@naver.com>
2024-05-31 10:22:45 +0900

    댓글 수정 화면 출력

--- src/component/comment/CommentItem.jsx
+++ src/component/comment/CommentItem.jsx
@@ -13,12 +13,17 @@ import {
   useToast,
 } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { faPenToSquare, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
+import { useContext, useState } from "react";
+import { LoginContext } from "../LoginProvider.jsx";
+import { CommentEdit } from "./CommentEdit.jsx";
 
 export function CommentItem({ comment, isProcessing, setIsProcessing }) {
+  const [isEditing, setIsEditing] = useState(false);
   const { isOpen, onClose, onOpen } = useDisclosure();
   const toast = useToast();
+  const account = useContext(LoginContext);
 
   function handleRemoveClick(id) {
     setIsProcessing(true);
@@ -46,28 +51,49 @@ export function CommentItem({ comment, isProcessing, setIsProcessing }) {
         <Spacer />
         <Box>{comment.inserted}</Box>
       </Flex>
-      <Flex>
-        <Box>{comment.comment}</Box>
-        <Spacer />
-        <Box>
-          <Button colorScheme={"red"} onClick={onOpen} isLoading={isProcessing}>
-            <FontAwesomeIcon icon={faTrashCan} />
-          </Button>
-        </Box>
-      </Flex>
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>댓글 삭제</ModalHeader>
-          <ModalBody>댓글을 삭제하시겠습니까?</ModalBody>
-          <ModalFooter>
-            <Button onClick={handleRemoveClick} colorScheme={"red"}>
-              삭제
-            </Button>
-            <Button onClick={onClose}>취소</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+      {isEditing || (
+        <Flex>
+          <Box>{comment.comment}</Box>
+          <Spacer />
+          {account.hasAccess(comment.memberId) && (
+            <Box>
+              <Button colorScheme={"blue"} onClick={() => setIsEditing(true)}>
+                <FontAwesomeIcon icon={faPenToSquare} />
+              </Button>
+              <Button
+                colorScheme={"red"}
+                onClick={onOpen}
+                isLoading={isProcessing}
+              >
+                <FontAwesomeIcon icon={faTrashCan} />
+              </Button>
+            </Box>
+          )}
+        </Flex>
+      )}
+      {isEditing && (
+        <CommentEdit
+          comment={comment}
+          setIsEditing={setIsEditing}
+          setIsProcessing={setIsProcessing}
+          isProcessing={isProcessing}
+        />
+      )}
+      {account.hasAccess(comment.memberId) && (
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>댓글 삭제</ModalHeader>
+            <ModalBody>댓글을 삭제하시겠습니까?</ModalBody>
+            <ModalFooter>
+              <Button onClick={handleRemoveClick} colorScheme={"red"}>
+                삭제
+              </Button>
+              <Button onClick={onClose}>취소</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      )}
     </Box>
   );
 }
